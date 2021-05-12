f88892f8d9d01b61bfb55f5d2084b9aac6796b94
Wiktor Sołtysiak <w.soltysiak@mymusic.pl>
2021-05-12 14:17:15 +0200

    Update

--- Licencjat.py
+++ Licencjat.py
@@ -6,15 +6,21 @@ from Token import Token
 import LoadCSV
 import Helpers
 nlp = sp.load("pl_core_news_sm")
+option = input()
+print("wybierz czy chcesz 1- wczytać nowe dane 2- wczytać już zserializowane dane")
+if option == 1:
+    Tweets_czas_decyzji = LoadCSV.LoadCzasDecyzji()
+    NAWL_BE = LoadCSV.LoadNAWL_BE()
+    Tweets = Helpers.SerializeDfTweetsToObjectsAndAssigneNawlValues(Tweets_czas_decyzji, NAWL_BE, nlp)
+    df = pd.DataFrame([x.as_dict() for x in Tweets])
+    df.to_csv("C:\\Users\\48500\\Desktop\\Licencjat\\czasdecyzji_TVP_TVN\\Tweety.csv", sep=";")
+elif option == 2: 
+    Tweets = LoadCSV.LoadTweets()
 
-Tweets_czas_decyzji = LoadCSV.LoadCzasDecyzji()
-NAWL_BE = LoadCSV.LoadNAWL_BE()
-
-Tweets = Helpers.SerializeDfTweetsToObjectsAndAssigneNawlValues(Tweets_czas_decyzji, NAWL_BE, nlp)
 
 
 
 
-df = pd.DataFrame([x.as_dict() for x in Tweets])
-df.to_csv("E:\\Lic\\Tweety.csv")
+
+
 stop = input()
--- LoadCSV.py
+++ LoadCSV.py
@@ -3,7 +3,7 @@ import spacy as sp
 
 
 def LoadCzasDecyzji():
-    dfTweets_czas_decyzji = pd.read_csv(filepath_or_buffer="E:\\Lic\\czasdecyzji_TVP_TVN.csv",
+    dfTweets_czas_decyzji = pd.read_csv(filepath_or_buffer="C:\\Users\\48500\\Desktop\\Licencjat\\czasdecyzji_TVP_TVN\\czasdecyzji_TVP_TVN.csv",
                         sep=";",
                         header=0,
                         usecols=[0,4,9,17],
@@ -18,10 +18,26 @@ def LoadCzasDecyzji():
     return dfTweets_czas_decyzji
 
 def LoadNAWL_BE():
-    NAWL_BE = pd.read_csv(filepath_or_buffer="E:\\Lic\\pone.0132305.s004.csv",
+    NAWL_BE = pd.read_csv(filepath_or_buffer="C:\\Users\\48500\\Desktop\\Licencjat\\czasdecyzji_TVP_TVN\\pone.0132305.s004.csv",
                         sep=";",
                         header=0,
                         usecols=[2,4,6,8,10,12],
+                        names=["slowo", "happiness", "ang", "sad","fear","disgust"],
+                        encoding="utf-8",
+                        dtype={"NAWL_word": str,
+                               "hap_M_all": float,
+                               "ang_M_all": float,
+                               "sad_M_all": float,
+                               "fea_M_all": float,
+                               "dis_M_all": float},
+                        low_memory= False,
+                        skip_blank_lines=True)
+    NAWL_BE= NAWL_BE.values.tolist()
+    return NAWL_BE
+
+def LoadTweets():
+    NAWL_BE = pd.read_csv(filepath_or_buffer="C:\\Users\\48500\\Desktop\\Licencjat\\czasdecyzji_TVP_TVN\\Tweety.csv",
+                        sep=";",
                         names=["slowo", "happiness", "ang", "sad","fear","disgust"],
                         encoding="utf-8",
                         dtype={"NAWL_word": str,
